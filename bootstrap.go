e1432191094e8e3ea890f3b35429dc9cd4aec2e6
=Rafael <rafael@one-shift.com>
2020-08-25 20:48:54 +0100

    Added GO html template engine + views dir structure

--- bootstrap.go
+++ bootstrap.go
@@ -2,13 +2,22 @@ package main
 
 import(
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/template/html"
 )
 
 func main() {
-	app := fiber.New()
+
+	//Declare the GO html template engine
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(&fiber.Settings{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello, World")
+		c.Render("index", fiber.Map{
+			"HelloWorld": "Welcome to Fiber!",
+		})
 	})
 
 	app.Listen(3000)
