426722e59b956314f80ea805921952ae5d7d9664
=Rafael <rafael@one-shift.com>
2020-08-25 21:05:08 +0100

    Routes update

--- routes/web.go
+++ routes/web.go
@@ -1,10 +1,10 @@
-package web
+package routes
 
 import(
 	"github.com/gofiber/fiber"
 )
 
-func router(app *fiber) {
+func web(app *fiber) {
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Render("index", fiber.Map{
 			"HelloWorld": "Welcome to Fiber!",
